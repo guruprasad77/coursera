f178077a048c3a4d9f5ea099eb789c6b947e9a6f
guruprasad77 <guruprasadkv183gmail.com>
2020-07-11 09:51:40 +0530

    assignments.

---
+++ mod-4/script.js
@@ -0,0 +1,17 @@
+
+(function () {
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i = 0; i < names.length; i++) {
+// Look up these methods on Mozilla Developer Network web site if needed.
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  if (firstLetter === 'j') {
+    byeSpeaker.speak(names[i]);
+  } else {
+    helloSpeaker.speak(names[i]);
+  }
+}
+
+})();
